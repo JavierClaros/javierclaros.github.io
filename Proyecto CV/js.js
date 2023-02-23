109a86e217647d60c759cd6be09858f5176de8ce
JavierClaros <126189640+JavierClaros@users.noreply.github.com>
2023-02-23 19:27:10 -0300

    Update js.js

--- Proyecto CV/js.js
+++ Proyecto CV/js.js
@@ -1,16 +1,19 @@
 function mostrarEstudios() {
-    var estudios = document.getElementById("estudios");
-    var boton = document.getElementById("boton-estudios");
-    if (estudios != null) {
-      if (estudios.style.display == "none") {
-        estudios.style.display = "block";
-        boton.textContent = "Ocultar contenido";
-      } else {
-        estudios.style.display = "none";
-        boton.textContent = "Mostrar contenido";
-      }
+  console.log("Función mostrarEstudios() llamada.");
+  var estudios = document.getElementById("estudios");
+  var boton = document.getElementById("boton-estudios");
+  if (estudios != null) {
+    var estilo = window.getComputedStyle(estudios);
+    if (estilo.display === "none") {
+      estudios.style.display = "block";
+      boton.textContent = "Ocultar contenido";
+    } else {
+      estudios.style.display = "none";
+      boton.textContent = "Mostrar contenido";
     }
   }
+}
+
   
     
     
